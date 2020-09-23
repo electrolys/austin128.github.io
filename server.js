27f791ccfc8d6bdfd9067a6fd46ebb22fb8dc8f1
electrolys <47481369+electrolys@users.noreply.github.com>
2020-09-23 14:00:35 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies.
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('n', function() {
+    players[socket.id] = {
+      x: 0.0,
+      y: 0.0
+    };
+  });
+  socket.on('u', function(data) {
+    //var player = players[socket.id];
+    players[socket.id] = data;
+    
+    //console.log("update");
+
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('s', players);
+}, 1000 / 60);
